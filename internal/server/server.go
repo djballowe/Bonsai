471e0d09683f1be474877baf071df4f47e610fef
djballowe <djballowe@gmail.com>
2026-03-22 16:10:19 -0700

    add all possible fields to UI for now

--- internal/server/server.go
+++ internal/server/server.go
@@ -77,21 +77,30 @@ func handleEvents(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// This is cringe but whatever for now
 func merge(last *printer.PrinterState, current *printer.PrinterState) *printer.PrinterState {
 	if last == nil {
 		return current
 	}
 	merged := *last
 
+	// Temperatures
 	if current.NozzleTemp != nil {
 		merged.NozzleTemp = current.NozzleTemp
+	}
+	if current.NozzleTargetTemp != nil {
+		merged.NozzleTargetTemp = current.NozzleTargetTemp
 	}
 	if current.BedTemp != nil {
 		merged.BedTemp = current.BedTemp
 	}
+	if current.BedTargetTemp != nil {
+		merged.BedTargetTemp = current.BedTargetTemp
+	}
 	if current.ChamberTemp != nil {
 		merged.ChamberTemp = current.ChamberTemp
 	}
+	// Print progress
 	if current.GcodeState != nil {
 		merged.GcodeState = current.GcodeState
 	}
@@ -101,14 +110,105 @@ func merge(last *printer.PrinterState, current *printer.PrinterState) *printer.P
 	if current.RemainingTime != nil {
 		merged.RemainingTime = current.RemainingTime
 	}
+	if current.PrintStage != nil {
+		merged.PrintStage = current.PrintStage
+	}
+	if current.PrintSubStage != nil {
+		merged.PrintSubStage = current.PrintSubStage
+	}
+	if current.PrintLineNum != nil {
+		merged.PrintLineNum = current.PrintLineNum
+	}
+	if current.LayerNum != nil {
+		merged.LayerNum = current.LayerNum
+	}
 	if current.TotalLayerNum != nil {
 		merged.TotalLayerNum = current.TotalLayerNum
+	}
+	if current.PrintType != nil {
+		merged.PrintType = current.PrintType
 	}
 	if current.PrintError != nil {
 		merged.PrintError = current.PrintError
 	}
+	// File / task
+	if current.GcodeFile != nil {
+		merged.GcodeFile = current.GcodeFile
+	}
+	if current.GcodeFilePrepercent != nil {
+		merged.GcodeFilePrepercent = current.GcodeFilePrepercent
+	}
+	if current.SubtaskName != nil {
+		merged.SubtaskName = current.SubtaskName
+	}
+	if current.SubtaskID != nil {
+		merged.SubtaskID = current.SubtaskID
+	}
+	if current.TaskID != nil {
+		merged.TaskID = current.TaskID
+	}
+	if current.ProjectID != nil {
+		merged.ProjectID = current.ProjectID
+	}
+	if current.ProfileID != nil {
+		merged.ProfileID = current.ProfileID
+	}
+	// Fans
+	if current.CoolingFanSpeed != nil {
+		merged.CoolingFanSpeed = current.CoolingFanSpeed
+	}
+	if current.HeatbreakFanSpeed != nil {
+		merged.HeatbreakFanSpeed = current.HeatbreakFanSpeed
+	}
+	if current.BigFan1Speed != nil {
+		merged.BigFan1Speed = current.BigFan1Speed
+	}
+	if current.BigFan2Speed != nil {
+		merged.BigFan2Speed = current.BigFan2Speed
+	}
+	if current.FanGear != nil {
+		merged.FanGear = current.FanGear
+	}
+	// Speed
+	if current.SpeedMagnitude != nil {
+		merged.SpeedMagnitude = current.SpeedMagnitude
+	}
+	if current.SpeedLevel != nil {
+		merged.SpeedLevel = current.SpeedLevel
+	}
+	// Hardware
+	if current.NozzleDiameter != nil {
+		merged.NozzleDiameter = current.NozzleDiameter
+	}
+	if current.NozzleType != nil {
+		merged.NozzleType = current.NozzleType
+	}
+	if current.SDCard != nil {
+		merged.SDCard = current.SDCard
+	}
 	if current.WifiSignal != nil {
 		merged.WifiSignal = current.WifiSignal
+	}
+	// AMS
+	if current.AMSStatus != nil {
+		merged.AMSStatus = current.AMSStatus
+	}
+	if current.AMSRFIDStatus != nil {
+		merged.AMSRFIDStatus = current.AMSRFIDStatus
+	}
+	if len(current.AMS.AMSList) > 0 {
+		merged.AMS = current.AMS
+	}
+	// Lights
+	if len(current.LightsReport) > 0 {
+		merged.LightsReport = current.LightsReport
+	}
+	// Queue
+	if current.QueueNumber != nil {
+		merged.QueueNumber = current.QueueNumber
+	}
+	if current.QueueTotal != nil {
+		merged.QueueTotal = current.QueueTotal
 	}
 	return &merged
 }
--- internal/view/view.go
+++ internal/view/view.go
@@ -6,45 +6,94 @@ import (
 )
 
 type StatusView struct {
-	NozzleTemp    string
-	BedTemp       string
-	ChamberTemp   string
+	// Temperatures
+	NozzleTemp       string
+	NozzleTargetTemp string
+	BedTemp          string
+	BedTargetTemp    string
+	ChamberTemp      string
+	// Progress
 	GcodeState    string
 	PrintPercent  string
 	RemainingTime string
-	TotalLayerNum string
 	LayerNum      string
+	TotalLayerNum string
+	PrintType     string
 	PrintError    string
-	WifiSignal    string
+	// File / Task
+	GcodeFile   string
+	SubtaskName string
+	TaskID      string
+	// Fans
+	CoolingFanSpeed   string
+	HeatbreakFanSpeed string
+	BigFan1Speed      string
+	BigFan2Speed      string
+	// Speed
+	SpeedMagnitude string
+	SpeedLevel     string
+	// Hardware
+	NozzleDiameter string
+	NozzleType     string
+	SDCard         string
+	WifiSignal     string
+	// Queue
+	QueueNumber string
+	QueueTotal  string
 }
 
 func NewStatusView(s *printer.PrinterState) StatusView {
 	v := StatusView{
-		NozzleTemp:    "--",
-		BedTemp:       "--",
-		ChamberTemp:   "--",
-		GcodeState:    "--",
-		PrintPercent:  "--",
-		RemainingTime: "--",
-		TotalLayerNum: "--",
-		LayerNum:      "--",
-		PrintError:    "none",
-		WifiSignal:    "--",
+		NozzleTemp:        "--",
+		NozzleTargetTemp:  "--",
+		BedTemp:           "--",
+		BedTargetTemp:     "--",
+		ChamberTemp:       "--",
+		GcodeState:        "--",
+		PrintPercent:      "--",
+		RemainingTime:     "--",
+		LayerNum:          "--",
+		TotalLayerNum:     "--",
+		PrintType:         "--",
+		PrintError:        "none",
+		GcodeFile:         "--",
+		SubtaskName:       "--",
+		TaskID:            "--",
+		CoolingFanSpeed:   "--",
+		HeatbreakFanSpeed: "--",
+		BigFan1Speed:      "--",
+		BigFan2Speed:      "--",
+		SpeedMagnitude:    "--",
+		SpeedLevel:        "--",
+		NozzleDiameter:    "--",
+		NozzleType:        "--",
+		SDCard:            "--",
+		WifiSignal:        "--",
+		QueueNumber:       "--",
+		QueueTotal:        "--",
 	}
 
 	if s == nil {
 		return v
 	}
 
+	// Temperatures
 	if s.NozzleTemp != nil {
 		v.NozzleTemp = fmt.Sprintf("%.1f°C", *s.NozzleTemp)
+	}
+	if s.NozzleTargetTemp != nil {
+		v.NozzleTargetTemp = fmt.Sprintf("%.1f°C", *s.NozzleTargetTemp)
 	}
 	if s.BedTemp != nil {
 		v.BedTemp = fmt.Sprintf("%.1f°C", *s.BedTemp)
 	}
+	if s.BedTargetTemp != nil {
+		v.BedTargetTemp = fmt.Sprintf("%.1f°C", *s.BedTargetTemp)
+	}
 	if s.ChamberTemp != nil {
 		v.ChamberTemp = fmt.Sprintf("%.1f°C", *s.ChamberTemp)
 	}
+	// Progress
 	if s.GcodeState != nil {
 		v.GcodeState = *s.GcodeState
 	}
@@ -54,17 +103,67 @@ func NewStatusView(s *printer.PrinterState) StatusView {
 	if s.RemainingTime != nil {
 		v.RemainingTime = fmt.Sprintf("%d min", *s.RemainingTime)
 	}
+	if s.LayerNum != nil {
+		v.LayerNum = fmt.Sprintf("%d", *s.LayerNum)
+	}
 	if s.TotalLayerNum != nil {
 		v.TotalLayerNum = fmt.Sprintf("%d", *s.TotalLayerNum)
 	}
-	if s.LayerNum != nil {
-		v.LayerNum = fmt.Sprintf("%d", *s.LayerNum)
+	if s.PrintType != nil {
+		v.PrintType = *s.PrintType
 	}
 	if s.PrintError != nil && *s.PrintError != 0 {
 		v.PrintError = fmt.Sprintf("%d", *s.PrintError)
 	}
+	// File / Task
+	if s.GcodeFile != nil {
+		v.GcodeFile = *s.GcodeFile
+	}
+	if s.SubtaskName != nil {
+		v.SubtaskName = *s.SubtaskName
+	}
+	if s.TaskID != nil {
+		v.TaskID = *s.TaskID
+	}
+	// Fans
+	if s.CoolingFanSpeed != nil {
+		v.CoolingFanSpeed = *s.CoolingFanSpeed
+	}
+	if s.HeatbreakFanSpeed != nil {
+		v.HeatbreakFanSpeed = *s.HeatbreakFanSpeed
+	}
+	if s.BigFan1Speed != nil {
+		v.BigFan1Speed = *s.BigFan1Speed
+	}
+	if s.BigFan2Speed != nil {
+		v.BigFan2Speed = *s.BigFan2Speed
+	}
+	// Speed
+	if s.SpeedMagnitude != nil {
+		v.SpeedMagnitude = fmt.Sprintf("%d%%", *s.SpeedMagnitude)
+	}
+	if s.SpeedLevel != nil {
+		v.SpeedLevel = fmt.Sprintf("%d", *s.SpeedLevel)
+	}
+	// Hardware
+	if s.NozzleDiameter != nil {
+		v.NozzleDiameter = *s.NozzleDiameter
+	}
+	if s.NozzleType != nil {
+		v.NozzleType = *s.NozzleType
+	}
+	if s.SDCard != nil {
+		v.SDCard = fmt.Sprintf("%t", *s.SDCard)
+	}
 	if s.WifiSignal != nil {
 		v.WifiSignal = *s.WifiSignal
+	}
+	// Queue
+	if s.QueueNumber != nil {
+		v.QueueNumber = fmt.Sprintf("%d", *s.QueueNumber)
+	}
+	if s.QueueTotal != nil {
+		v.QueueTotal = fmt.Sprintf("%d", *s.QueueTotal)
 	}
 
 	return v
